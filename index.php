93f628d62b18922e37b798f28390aa7b58de6e09
gtorres99 <ingelectronicagt99@gmail.com>
2023-05-04 21:04:44 -0500

    tipos de datos php 4-5-2023

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-    $nombre = "Gustavo Torres";
-    $Nombre = "otro nombre"
+   $cadena = "Hola mundo mi nombre es: 'Gustavo' y tengo\"29\" años de edad;
+   echo $cadena;
+   
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?php echo "hola mundo mi nombre es $Nombre "; ?></h1>
-</body>
-</html>
